80341360312914c93af28dfd32a07d1cda90183e
YaroslavRomanenko <yaroslavromanenko456@gmail.com>
2025-10-15 19:57:36 +0300

    feat: add Interpreter class for code execution and RuntimeError for handling runtime errors

---
+++ src/com/yarikcompany/potujno/RuntimeError.java
@@ -0,0 +1,10 @@
+package com.yarikcompany.potujno;
+
+public class RuntimeError extends RuntimeException {
+    final Token token;
+
+    RuntimeError(Token token, String message) {
+        super(message);
+        this.token = token;
+    }
+}
