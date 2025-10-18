dd006eef2bf8c8df78bd6c81c78ab46db2a00a13
YaroslavRomanenko <yaroslavromanenko456@gmail.com>
2025-10-18 12:02:44 +0300

    feat: add resolver

--- src/com/yarikcompany/potujno/Potujno.java
+++ src/com/yarikcompany/potujno/Potujno.java
@@ -56,6 +56,11 @@ public class Potujno {
 
         if (hadError) return;
 
+        Resolver resolver = new Resolver(interpreter);
+        resolver.resolve(statements);
+
+        if (hadError) return;
+
         interpreter.interpret(statements);
     }
 
